4122e39a7cbc12e1e8dacecc5b3b13420ff81fbc
James <james.ryan.mcguire@gmail.com>
2021-08-06 13:31:56 -0700

    Adds exercises routes and controller actions index and show

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,2 +1,28 @@
 class ExercisesController < ApplicationController
+  
+  def index
+    exercises = Exercise.all
+    render json: exercises.as_json
+  end
+
+  def show
+    exercise_id = params["id"]
+    exercise = Exercise.find(exercise_id)
+    render json: exercise
+  end
+
+  # def create
+  #   exercise = Exercise.new(
+  #     name: params["name"],
+  #     muscle_group: params["muscle_group"]
+  #     instructions: params["instructions"],
+  #     image: params["image"],
+  #   )
+  #   if exercise.save
+  #     render json: exercise.as_json
+  #   else 
+  #     render json: {errors: exercise.errors.full_messages}, 
+  #     status: 422
+  #   end
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,21 @@ Rails.application.routes.draw do
   ## Users
 
   get "/users" => "users#index"
-
   post "/users" => "users#create"
+
+  ## Exercises
+
+  get "/exercises" => "exercises#index"
+  get "/exercises/:id" => "exercises#show"
+  post "/exercises" => "exercises#create"
+  patch "/exercises/:id" => "exercises#update"
+  delete "/exercises/:id" => "exercises#destroy"
+
+
+
+
+
+
+
+  ## Workouts
 end
