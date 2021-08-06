d2a5ac6e96a190acc680983f37ff16bb654fcc16
James <james.ryan.mcguire@gmail.com>
2021-08-06 12:15:17 -0700

    Changes exercise intstructions ttribute from string to text

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_04_223115) do
+ActiveRecord::Schema.define(version: 2021_08_06_191235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2021_08_04_223115) do
   create_table "exercises", force: :cascade do |t|
     t.string "name"
     t.string "muscle_group"
-    t.string "instructions"
+    t.text "instructions"
     t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
