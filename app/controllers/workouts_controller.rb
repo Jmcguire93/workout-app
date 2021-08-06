e54fc80fb3958d5b060635e5210c158a98d87e69
James <james.ryan.mcguire@gmail.com>
2021-08-06 13:41:51 -0700

    Adds workouts CRUD routes and actions

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,2 +1,51 @@
 class WorkoutsController < ApplicationController
+
+  def index
+    workouts = Workout.all
+    render json: workouts.as_json
+  end
+
+  def show
+    workout_id = params["id"]
+    workout = Workout.find(workout_id)
+    render json: workout
+  end
+
+  def create
+    workout = Workout.new(
+      name: params["name"],
+      description: params["description"]
+      user_id: params["user_id"],
+    )
+    if workout.save
+      render json: workout.as_json
+    else 
+      render json: {errors: workout.errors.full_messages}, 
+      status: 422
+    end
+  end
+
+  def update
+    workout_id = params[:id]
+    workout = Workout.find_by(id: workout_id)
+
+    workout.name = params["name"] || workout.name
+    workout.description = params["description"] || workout.description
+    # workout.user_id = params["user_id"] || workout.user_id
+
+    if workout.save
+      render json: workout.as_json
+    else
+      render json: {errors: workout.errors.full_messages}, 
+      status: 422
+    end
+  end
+
+  def destroy
+    workout_id = params[:id]
+    workout = Workout.find_by(id: workout_id)
+    workout.destroy
+    render json: {message: "Workout successfully destroyed!"}    
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -12,11 +12,11 @@ Rails.application.routes.draw do
   patch "/exercises/:id" => "exercises#update"
   delete "/exercises/:id" => "exercises#destroy"
 
-
-
-
-
-
-
   ## Workouts
+
+  get "/workouts" => "workouts#index"
+  get "/workouts/:id" => "workouts#show"
+  post "/workouts" => "workouts#create"
+  patch "/workouts/:id" => "workouts#update"
+  delete "/workouts/:id" => "workouts#destroy"
 end
