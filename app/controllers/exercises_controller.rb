0d13c9b7949ee5b4380f7d65e87a35e7411a356e
James <james.ryan.mcguire@gmail.com>
2021-08-09 13:12:23 -0700

    Fixes render syntax for exercises controller action show

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -8,29 +8,46 @@ class ExercisesController < ApplicationController
   def show
     exercise_id = params["id"]
     exercise = Exercise.find(exercise_id)
-    render json: exercise
+    render json: exercise.as_json
   end
   
-  # #Testing actions, we don't want users to manipulate the exercise database.
-  # def create
-  #   exercise = Exercise.new(
-  #     name: params["name"],
-  #     muscle_group: params["muscle_group"],
-  #     instructions: params["instructions"],
-  #     image: params["image"],
-  #   )
-  #   if exercise.save
-  #     render json: exercise.as_json
-  #   else 
-  #     render json: {errors: exercise.errors.full_messages}, 
-  #     status: 422
-  #   end
-  # end
+#   #Testing actions, we don't want users to manipulate the exercise database.
+#   def create
+#     exercise = Exercise.new(
+#       name: params["name"],
+#       muscle_group: params["muscle_group"],
+#       instructions: params["instructions"],
+#       image: params["image"],
+#     )
+#     if exercise.save
+#       render json: exercise.as_json
+#     else 
+#       render json: {errors: exercise.errors.full_messages}, 
+#       status: 422
+#     end
+#   end
 
-  # def destroy
-  #   exercise_id = params[:id]
-  #   exercise = Exercise.find_by(id: exercise_id)
-  #   exercise.destroy
-  #   render json: {message: "Exercise successfully destroyed!"}    
-  # end
+#   def update
+#     exercise_id = params[:id]
+#     exercise = Exercise.find_by(id: exercise_id)
+
+#     exercise.name = params["name"] || exercise.name
+#     exercise.muscle_group = params["muscle_group"] || exercise.muscle_group
+#     exercise.instructions = params["instructions"] || exercise.instructions
+#     exercise.image = params["image"] || exercise.image
+
+#     if exercise.save
+#       render json: exercise.as_json
+#     else
+#       render json: {errors: exercise.errors.full_messages}, 
+#       status: 422
+#     end
+#   end
+
+#   def destroy
+#     exercise_id = params[:id]
+#     exercise = Exercise.find_by(id: exercise_id)
+#     exercise.destroy
+#     render json: {message: "Exercise successfully destroyed!"}    
+#   end
 end
