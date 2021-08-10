972a325248b9d1a95e129e14e161c7655eaae102
James <james.ryan.mcguire@gmail.com>
2021-08-09 17:10:02 -0700

    Comments out user validations and adds more data to seeds file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :name, length: {maximum: 20}
-  validates :password, length: {in: 6..20}  
-  validates :email, presence: true, uniqueness: true
+  # validates :name, length: {maximum: 20}
+  # validates :password, length: {in: 6..20}  
+  # validates :email, presence: true, uniqueness: true
 
   has_many :workouts 
   # has_many :reviews
